3badedcd8482601e3863fd0696cb51ae10a25731
Jason Stallings <jason@stallin.gs>
2021-01-20 16:42:05 -0600

    wip: Add exit logic.
    Co-Authored-By: Brandon Geraci <25802530+Atr2600@users.noreply.github.com>

--- ArduCopter/mode.h
+++ ArduCopter/mode.h
@@ -1383,6 +1383,7 @@ public:
 
     bool init(bool ignore_checks) override;
     void run() override;
+    void exit() override;
 
     bool requires_GPS() const override { return false; }
     bool has_manual_throttle() const override { return true; }
--- ArduCopter/mode_turtle.cpp
+++ ArduCopter/mode_turtle.cpp
@@ -1,4 +1,5 @@
 #include "Copter.h"
+#include "mode.h"
 
 #if MODE_TURTLE_ENABLED == ENABLED
 
@@ -6,11 +7,22 @@
  * Init and run calls for turtle flight mode
  */
 
+
 // turtle_init - initialise turtle controller
 bool ModeTurtle::init(bool ignore_checks)
-{
+{   
+
+    #if FRAME_CONFIG == HELI_FRAME
+        // do not allow helis to use turtle mode
+        return false;
+    #endif
+
+    // we don't want the motors enabled while upside down.
+    copter.air_mode = AirMode::AIRMODE_DISABLED;
+
+
     // set target to current position
-    // TODO: Run initialization like reversing motors? 
+    // TODO: Run dshot command to reverse motors? 
 
     // DSHOT command 20 will set the motor direction to normal, and 21 will reverse.
     return true;
@@ -20,6 +32,16 @@ bool ModeTurtle::init(bool ignore_checks)
 void ModeTurtle::run()
 {
    // TODO: Check stick position and move the corresponding motor.
+}
+
+void ModeTurtle::exit()
+{
+    // TODO: Set airmode to it's previous state.
+    //  if (g2.acro_options.get() & uint8_t(AcroOptions::AIR_MODE)) {
+    //     copter.air_mode = AirMode::AIRMODE_ENABLED;
+    // }
+
+    // TODO: Use DSHOT commant to set motor direction to normal.
 }
 
 #endif
